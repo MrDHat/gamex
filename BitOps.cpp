bff1fbdcbedf81fe922b4bddeda47deaa0a692fb
Aman Madaan <madaan.amanmadaan@gmail.com>
2012-09-26 01:55:06 +0530

    Added BitOps

---
+++ BitOps.cpp
@@ -0,0 +1,26 @@
+//sg
+#include<iostream>
+class BitOps
+{
+/*this class should enclose the 
+ * parser and the functions 
+ * 1. And ,or ,not ,xor,
+ * 2. ones (x) : returns number of 1's in binary representation of x
+ *  similar to ones, define functions like : 
+ * zeros(x)
+ * 3. firstOne(x) : returns location of first one log2(x&(-x))
+ * 4. nearPower(x) : nearest power of x
+ * 
+ * Just implement these functions and after that, 
+ * you need to provide the following functionality : 
+ * 1. You need to parse what user has entered. 
+ * 2. if user types "help" just dump all the commands available
+ * 3. if user types exit, go to main screen ( for now ,exit the program)
+ * 4. if user types a valid command , print result in the next line. 
+ * eg : 
+ * 		$help
+ * 		$ ones : get number of ones in binary representaion
+ *   ( the dollar will also be printed by your program)
+ * 	
+ */ 
+}
