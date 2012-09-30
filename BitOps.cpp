016afef55817e89e5cc477b35fa30952a1d56f5a
MrDHat <ktyaks@gmail.com>
2012-09-30 14:28:55 +0530

    Enhanced Functionality, Added Exit/help

--- BitOps.cpp
+++ BitOps.cpp
@@ -8,6 +8,10 @@
 
 using namespace std ;
 
+
+// Giving Invalid Input when " " 
+// And,Or,not,XOR left
+
 class BitOps
 {
 /*this class should enclose the 
@@ -33,6 +37,30 @@ class BitOps
  */ 
 
 public :
+
+int And(int x, int y){
+  return x & y;
+}
+ 
+int Or(int x, int y){
+  return x | y;
+}
+ 
+int Xor(int x, int y){
+  return x ^ y;
+}
+ 
+int Not(int x){
+  return !x ;
+}
+
+
+void Exit(){
+  
+  clear();
+  endwin();
+  exit(0);
+}
 
 int ones(int x)			// FUNCTION TO CALCULATE THE NUMBER OF ONEs IN BINARY REPRESENTATION OF X
 {
@@ -97,21 +125,30 @@ int nearPower(int x){	// Calculating nearest power of 2
   x=x|(x>>4);
   x=x|(x>>8);
   x=x|(x>>16);
-//   x=x|(x>>32);
   ++x;
   return x;
 }
 
+// -1 => Invalid Input
+// -2 => help
+// -3 => exit
 
-int parser(char *temp){						// FUNCTION FOR PERFORMING THE PARSING OF THE INPUT
+int parser(char *temp){						// FUNCTION FOR PARSING THE INPUT
     string msg(temp) ;
-    size_t brack1, brack2 ;
+    size_t brack1, brack2, length_of_msg ;
     int num ;
+    
+    length_of_msg = msg.length();
     
     if(msg.find("ones")!=string::npos)				// CHECKING FOR ones FUNCTION
     {
       brack1 = msg.find("(") ;
       brack2 = msg.find(")") ;
+      
+      
+      if(4 + brack2 - brack1 + 1 != length_of_msg)		// Checking the length of Input
+	return -1 ;
+      
       if(brack1!=string::npos || brack2!=string::npos)
       {
 	num = atoi(msg.substr(brack1+1,brack2).c_str()) ;
@@ -125,6 +162,8 @@ int parser(char *temp){						// FUNCTION FOR PERFORMING THE PARSING OF THE INPUT
     {
       brack1 = msg.find("(") ;
       brack2 = msg.find(")") ;
+      
+      
       if(brack1!=string::npos || brack2!=string::npos)
       {
 	num = atoi(msg.substr(brack1+1,brack2).c_str()) ;
@@ -138,6 +177,8 @@ int parser(char *temp){						// FUNCTION FOR PERFORMING THE PARSING OF THE INPUT
     {
       brack1 = msg.find("(") ;
       brack2 = msg.find(")") ;
+      
+   
       if(brack1!=string::npos || brack2!=string::npos)
       {
 	num = atoi(msg.substr(brack1+1,brack2).c_str()) ;
@@ -151,6 +192,8 @@ int parser(char *temp){						// FUNCTION FOR PERFORMING THE PARSING OF THE INPUT
     {
       brack1 = msg.find("(") ;
       brack2 = msg.find(")") ;
+      
+      
       if(brack1!=string::npos || brack2!=string::npos)
       {
 	num = atoi(msg.substr(brack1+1,brack2).c_str()) ;
@@ -160,18 +203,17 @@ int parser(char *temp){						// FUNCTION FOR PERFORMING THE PARSING OF THE INPUT
 	return -1 ;
     }
     
-    else return -1 ;							// INVALID INPUT
+    else if(msg.find("help")!=string::npos && length_of_msg == 4){				// need help?
+      return -2;
+    }
+    else if(msg.find("exit")!=string::npos && length_of_msg == 4)  // Exit entered
+      Exit();
+    else 
+      return -1 ;							// INVALID INPUT
   }
 };
 
-
-int main(){
-  
-  BitOps obj  ;
-  
-  int height, width, i = 0, status ;
-  char msg[20] ;
-//   string msg;
+void initWin(int height, int width){	// Function to initialize Window
   
   initscr();
   clear();
@@ -181,22 +223,48 @@ int main(){
   start_color();
   init_pair(1, COLOR_RED, COLOR_BLACK);
   init_pair(2, COLOR_WHITE, COLOR_BLACK);
+}
+
+int main(){
+  
+  BitOps obj  ;
+  
+  int height, width, i = 0, status ;
+  char msg[20], ch ;
+  
+  initWin(height, width);
     
-//    box(stdscr, 0, 0) ;
-//   while(1){
+  while(1){
     
     attron(COLOR_PAIR(1)|A_BOLD);
     mvprintw(i, 0,"[gamex] $ ");
     attroff(COLOR_PAIR(1));
     getstr(msg);
-    status = obj.parser(msg) ;
-    if(status!=-1)
-      mvprintw(++i, 0, "%d", status) ;
-    else
-      mvprintw(++i, 0, "Invalid Input!") ;
     
-//   }
-  
+    if(strcmp(msg,"\n") == 0){				// Check for Enter Key
+    }  
+    else{
+	status = obj.parser(msg) ;
+      if(status!=-1){
+	if(status == -2){				// Help Detected
+	  mvprintw(++i, 0, "ones(x): Prints the number of ones in binary representation of x") ;
+	  mvprintw(++i, 0, "zeros(x): Prints the number of zeroes in binary representation of x") ;
+	  mvprintw(++i, 0, "nearPower(x) : prints the nearest power of 2 closest to x") ;
+	  mvprintw(++i, 0, "firstOne(x):  Prints the mask of number x (Location of first one from LSB)") ;
+	}
+	else 
+	  mvprintw(++i, 0, "%d", status) ;
+      }
+      else
+	mvprintw(++i, 0, "Invalid Input!") ;  
+    }
+    ++i;
+   if(i == LINES + 1|| i == LINES - 1 || i == LINES + 2){
+      i = 0;
+      refresh();
+      clear(); 
+   }
+  }  
   refresh();
   getch();
   clear();
